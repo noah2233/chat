22e954011c119be9cdc982c6744837280ec27221
Noah Lubko <nlubko@gmail.com>
2019-07-27 15:04:21 +0300

    add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+// shared
+import { AppHeaderComponent } from '@shared/app-header/app-header.component';
+// services
+import { CommonService } from '@services/common.service';
+import { AuthService } from '@services/auth.service';
+import { ChatService } from '@services/chat.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CommonService, AuthService, ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
