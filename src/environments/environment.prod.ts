fecce48083e92c178de5593f2e40aeaa1b8ee306
Noah Lubko <nlubko@gmail.com>
2019-07-27 15:36:20 +0300

    add firebase enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  firebase: {}
+  firebase: {
+    apiKey: 'AIzaSyD5CzpFS1DsylKNKwuyGv-RNQidH6b-g3I',
+    authDomain: 'chat-11536.firebaseapp.com',
+    databaseURL: 'https://chat-11536.firebaseio.com',
+    projectId: 'chat-11536',
+    storageBucket: '',
+    messagingSenderId: '1014352647365',
+    appId: '1:1014352647365:web:df5fd9770ab3b36e'
+  }
 };
