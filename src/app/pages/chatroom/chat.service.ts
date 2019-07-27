191aa91ba5fa62eecf5c8a70971b64a5d7a85454
Noah Lubko <nlubko@gmail.com>
2019-07-27 17:09:38 +0300

    add model

--- src/app/pages/chatroom/chat.service.ts
+++ src/app/pages/chatroom/chat.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 
-
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
@@ -8,6 +7,8 @@ import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs';
 
 import { AuthService } from '@services/auth.service';
+
+import { ChatMessage } from '@pages/chatroom/models/chat-messages.model';
 
 @Injectable({
   providedIn: 'root'
