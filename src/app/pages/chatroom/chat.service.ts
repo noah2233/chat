83148ea9c793810f9784f3b00d29f410ccfa5398
Noah Lubko <nlubko@gmail.com>
2019-07-29 09:15:57 +0300

    get sendMessage working

--- src/app/pages/chatroom/chat.service.ts
+++ src/app/pages/chatroom/chat.service.ts
@@ -5,7 +5,7 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { AuthService } from '@services/auth.service';
 
@@ -24,22 +24,24 @@ export class ChatService {
     private _angularFireDatabase: AngularFireDatabase,
     private _angularFireAuth: AngularFireAuth,
     private _authService: AuthService) {
-    this._angularFireAuth.authState
-      .subscribe(auth => {
-        if (auth !== undefined && auth !== null) {
-          this.user = auth;
-        }
-      });
+    // this._angularFireAuth.authState
+    //   .subscribe(auth => {
+    //     if (auth !== undefined && auth !== null) {
+    //       this.user = auth;
+    //     }
+    //   });
   }
 
   sendMessage(message: string) {
     const timeStamp = this.getTimeStamp();
-    const email = this.user.email;
+    // const email = this.user.email;
+    const email = 'user@email';
     this.chatMessages = this.getMessages();
     this.chatMessages.push({
       message: message,
       timeSent: timeStamp,
-      userName: this.userName,
+      // userName: this.userName,
+      userName: 'userName',
       email: email
     });
   }
--- src/app/pages/chatroom/models/chat-messages.model.ts
+++ src/app/pages/chatroom/models/chat-messages.model.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 export class ChatMessage {
     $key?: string;
     email?: string;
-    userName?: Observable<string>;
+    userName?: string;
     message?: string;
     timeSent?: string;
 }
