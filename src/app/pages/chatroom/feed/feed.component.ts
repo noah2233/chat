4915447246aac4559ebb2fd35e5cf79e0b38a154
Noah Lubko <nlubko@gmail.com>
2019-07-29 09:32:08 +0300

    add feed messages

--- src/app/pages/chatroom/feed/feed.component.ts
+++ src/app/pages/chatroom/feed/feed.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
+import { ChatService } from '@pages/chatroom/chat.service';
+
+import { AngularFireList } from 'angularfire2/database';
+
+import { Observable } from 'rxjs';
+
+import { ChatMessage } from '@pages/chatroom/models/chat-messages.model';
 @Component({
   selector: 'feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnChanges {
+  feed: AngularFireList<ChatMessage>;
 
-  constructor() { }
+  constructor(
+    private _chatService: ChatService) { }
 
   ngOnInit() {
+    this.feed = this._chatService.getMessages();
+  }
+
+  ngOnChanges() {
+    this.feed = this._chatService.getMessages();
   }
 
 }
