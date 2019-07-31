d962b0a7c9a660ef29777ab51e87b1f1417f9e66
Noah Lubko <nlubko@gmail.com>
2019-07-31 19:27:30 +0300

    add signup method

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { AuthService } from '@services/auth.service';
+
+import { pages } from '@core/consts';
 
 @Component({
   selector: 'signup',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  email: string;
+  password: string;
+  dispalyName: string;
+  errorMsg: string;
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _authService: AuthService) { }
 
   ngOnInit() {
+    this.initSignUpForm();
+  }
+
+  initSignUpForm() { }
+
+  signUp() {
+    const email = this.email;
+    const password = this.password;
+    const dispalyName = this.dispalyName;
+
+    this._authService.signUp(email, password, dispalyName).subscribe(() => {
+      this._router.navigate([pages.chat]);
+    }, error => {
+      this.errorMsg = error;
+    });
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+import { Observable } from 'rxjs';
+
+import { User } from '@pages/chatroom/models/user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private user: Observable<firebase.User>;
+  private authState: any;
 
-  constructor() { }
+  constructor(
+    private _httpClient: HttpClient,
+    private _angularFireDatabase: AngularFireDatabase,
+    private _angularFireAuth: AngularFireAuth,
+    private _router: Router) {
+    this.user = _angularFireAuth.authState;
+  }
+
+  signUp(email: string, password: string, displayName: string) {
+    return this._httpClient.get(null);
+  }
 }
