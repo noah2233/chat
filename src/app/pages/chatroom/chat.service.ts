5f5cb85e02115d5341c213e3bcace1a8a4ffe9e9
Noah Lubko <nlubko@gmail.com>
2019-08-01 19:00:23 +0300

    fix currentUserID

--- src/app/pages/chatroom/chat.service.ts
+++ src/app/pages/chatroom/chat.service.ts
@@ -15,33 +15,49 @@ import { ChatMessage } from '@pages/chatroom/models/chat-messages.model';
   providedIn: 'root'
 })
 export class ChatService {
-  user: any;
+  user: firebase.User;
   chatMessages: AngularFireList<ChatMessage>;
   chatMessage: ChatMessage;
-  userName: Observable<string>;
+  userName: string;
 
   constructor(
     private _angularFireDatabase: AngularFireDatabase,
     private _angularFireAuth: AngularFireAuth,
     private _authService: AuthService) {
-    // this._angularFireAuth.authState
-    //   .subscribe(auth => {
-    //     if (auth !== undefined && auth !== null) {
-    //       this.user = auth;
-    //     }
-    //   });
+    this._angularFireAuth.authState
+      .subscribe(auth => {
+        if (auth !== undefined && auth !== null) {
+          this.user = auth;
+        }
+
+        this.getUser().subscribe((result) => {
+          this.userName = result['displayName'];
+          console.log(result);
+        });
+      });
+  }
+
+  getUser() {
+    const userId = this.user.uid;
+    const path = `/users/${userId}`;
+
+    return this._angularFireDatabase.object(path).valueChanges();
+  }
+
+  getUsers() {
+    const path = `/users`;
+
+    return this._angularFireDatabase.list(path).valueChanges();
   }
 
   sendMessage(message: string) {
     const timeStamp = this.getTimeStamp();
-    // const email = this.user.email;
-    const email = 'user@email';
+    const email = this.user.email;
     this.chatMessages = this.getMessages();
     this.chatMessages.push({
       message: message,
       timeStamp: timeStamp,
-      // userName: this.userName,
-      userName: 'userName',
+      userName: this.userName,
       email: email
     });
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -25,7 +25,7 @@ export class AuthService {
   }
 
   get currentUserID() {
-    return this.authState !== null ? this.authState.uid : '';
+    return this.authState !== null ? this.authState.user.uid : '';
   }
 
   login(email: string, password: string) {
