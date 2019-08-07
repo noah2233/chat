478ba29aec7751b3ebcf7f3c41533fe9690c1504
Noah Lubko <nlubko@gmail.com>
2019-08-07 06:21:42 +0300

    add initUsersList

--- src/app/pages/chatroom/user-list/user-list.component.ts
+++ src/app/pages/chatroom/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ChatService } from '@pages/chatroom/chat.service';
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _chatService: ChatService) { }
 
   ngOnInit() {
+    this.initUsersList();
+  }
+
+  initUsersList() {
+    this._chatService.getUsers().subscribe(results => console.log(results));
   }
 
 }
